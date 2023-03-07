7752211c3cde68a1751095381acd6770210080e2
yuxxeun <itsyuxxeun@gmail.com>
2023-03-07 13:19:51 +0700

    init invero.pro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,12 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import vercel from "@astrojs/vercel/serverless";
+import sitemap from "@astrojs/sitemap";
+import prefetch from "@astrojs/prefetch";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://invero-pro.now.sh',
+  output: "server",
+  adapter: vercel(),
+  integrations: [sitemap(({ customPages: ['https://invero-pro.now.sh'] })), prefetch()],
+});
---
+++ src/config.mjs
@@ -0,0 +1,9 @@
+export const SITE = {
+  name: "testing",
+  brand: "Brand name",
+  description: "Description for brand.",
+  origin: "https://invero.pro",
+  basePathname: "/",
+  socialImage:
+    "https://github.com/yuxxeun/honeypod/blob/main/src/assets/images/gradient.jpg?raw=true",
+};
